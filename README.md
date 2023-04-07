5e52617396bcd53a0c66d58a87d023c27f7fa18a
MateusHBR <mateusmorais.r@gmail.com>
2023-04-07 13:24:34 -0300

    implement formatted files

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
 ## Formatador de arquivos
+Script utilitário utilizado para formatar arquivos
 
-[WATCH]
+O input será arquivos que nāo possuem 0 a esquerda, o resultado sempre será arquivos no seguinte padrāo:
+
+```
+CODE   REFERENCE
+```
+
+Descriçāo: 
+- CODE: Zeros a esquerda enquanto código menor que 13 colunas.  
+- REFERENCE: A descriçāo do código relacionado a aquele código
+
+Nesse projeto estāo os arquivos de exemplo mencionados em:
+input.txt - Representando o arquivo a ser convertido
+output.txt - Representando o arquivo resultante
+
+### [WATCH]
 ```
 cargo watch -x run
 ```
 
-[RUN]
+### [RUN]
 ```
 cargo run
 ```
 
-[BUILD]
+### [BUILD]
 ```
 cargo build --release
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,45 @@
-fn main() {
-    let input = "
-    123   Remédio
-    12356      Luva
-    12  Mochila
-    1 Bolsa
-    3 Bolsa de couro
-    "
-    .to_string();
+use std::env;
+use std::io::Write;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
-    let lines: Vec<String> = input.lines().map(format).collect();
-    lines.iter().for_each(|v| println!("{}", v));
+const ERROR_MESSAGE: &str = "FALHA AO CONVERTER LINHA";
+const INPUT_FILE: &str = "input.txt";
+const OUTPUT_FILE: &str = "output.txt";
+
+fn main() {
+    let current_dir = env::current_dir()
+        .expect("Falha ao encontrar o diretório atual")
+        .display()
+        .to_string();
+
+    let file: File = File::open(format!("{current_dir}/{INPUT_FILE}"))
+        .expect("Falha ao encontrar o arquivo especificado");
+
+    let reader = BufReader::new(&file);
+
+    let mut result_file =
+        File::create(format!("{current_dir}/{OUTPUT_FILE}")).expect("Falha ao criar o arquivo");
+
+    reader
+        .lines()
+        .map(|line| line.unwrap_or(ERROR_MESSAGE.to_string()))
+        .map(format)
+        .for_each(|line| write_in_file(line, &mut result_file));
 }
 
-fn format(line: &str) -> String {
+fn write_in_file(line: String, file: &mut File) {
+    file.write_all(format!("{}\r", line).as_bytes())
+        .expect("Falha ao escrever no arquivo");
+}
+
+fn format(line: String) -> String {
+    if line == ERROR_MESSAGE {
+        return line;
+    }
+
     let splitted: Vec<String> = line
         .trim()
         .split(" ")
